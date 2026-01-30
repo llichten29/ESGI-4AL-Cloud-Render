4f15b2b40e294d5fa0b0646768485ca8e3011c2b
llichten29 <llichten29@gmail.com>
2026-01-30 14:57:19 +0100

    fix; bug

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL
-  '',
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
